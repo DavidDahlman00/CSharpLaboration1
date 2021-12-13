2c31d085cb644ab2075260e7b5feaadb761445e3
David Dahlman <daviddahlman82@gmail.com>
2021-12-13 13:38:18 +0100

    change to singelton

---
+++ Laboration1/Controllers/AnimalController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laboration1.DTOs;
+using Laboration1.Entities;
+using Laboration1.Repo;
+using Microsoft.AspNetCore.Mvc;
+namespace Laboration1.Controllers
+{
+    [ApiController]
+    [Route("animal")]
+
+    public class AnimalController : ControllerBase
+    {
+        private readonly IAnimalRepo _repo;
+
+        public AnimalController(IAnimalRepo repo)
+        {
+            _repo = repo;
+        }
+
+   
+        //GET /animal
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAnimals()
+        {
+            var animals = _repo.GetAll();
+            var animals2 = animals.Select(v => new AnimalDTO
+            {
+                Id = v.Id,
+                Animaltype = v.Animaltype,
+                Name = v.Name
+            })
+                .OrderBy(x => x.Name);
+            return Ok(animals);
+            //List<Animal> animals = _repo.GetAll();
+            //return Ok(animals);
+        }
+
+
+        //GET /animal/:id
+
+     
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetAnimalByID(int id)
+        {
+            Animal animal = _repo.GetByID(id);
+            if (animal is null) // animal == null
+            {
+                return NotFound("Could not find animal with ID " + id);
+            }
+
+            AnimalDTO animalDTO = MapAnimalToAnimalDTO(animal);
+            return Ok(animalDTO);
+            //return _repo.GetByID(id);
+        }
+
+        [HttpPost("")]
+        public IActionResult CreateAnimal([FromBody] CreateAnimalDTO createAnimalDTO)
+        {
+
+            
+             Animal createdAnimal = _repo.CreateAnimal(createAnimalDTO);
+            AnimalDTO animalDTO = MapAnimalToAnimalDTO(createdAnimal);
+
+            return CreatedAtAction(
+                nameof(GetAnimalByID),
+                new { id = animalDTO.Id },
+                animalDTO);
+             
+
+
+            /*Animal createdAnimal = _repo.CreateAnimal(createAnimalDTO);
+            return CreatedAtAction(
+                nameof(GetAnimalByID),
+                new { id = createdAnimal.Id },
+                createdAnimal);*/
+
+        }
+
+        /*
+         public IActionResult CreateVinyl([FromBody]CreateVinylDTO createVinylDTO)
+        {
+            Vinyl createdVinyl = _repo.CreateVinyl(createVinylDTO);
+            return CreatedAtAction(
+                nameof(GetVinylByID),
+                new { id = createdVinyl.Id },
+                createdVinyl);
+            
+        }*/
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteVinyl(int id)
+        {
+            _repo.DeleteAnimal(id);
+            return NoContent();
+        }
+        /*
+        [HttpPut]
+        public Vinyl UpdateVinyl([FromBody] Vinyl vinyl, int id)
+        {
+            _repo.UpdateVinyl(vinyl, id);
+            return vinyl;
+        }*/
+
+        private AnimalDTO MapAnimalToAnimalDTO(Animal animal)
+        {
+            return new AnimalDTO
+            {
+                Id = animal.Id,
+                Animaltype = animal.Animaltype,
+                Name = animal.Name,
+            };
+        }
+    }
+}
---
+++ Laboration1/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Labb1.Repo;
+using Laboration1.Repo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Laboration1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IAnimalRepo, AnimalRepo>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
